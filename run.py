18e8ddbf3988f2590fe562406b1e12f57cf3b5f1
Parth Shah <parthks@umich.edu>
2017-05-18 02:10:39 +0530

    Use Selenium
    First iteration. All in a while True loop, around 10 steps.
    Pretty basic version. Think it works? Lets see...

--- run.py
+++ run.py
@@ -1,7 +1,101 @@
 import wget
 
-final = 'https://r10---sn-h557snes.googlevideo.com/videoplayback?id=5b372923ff879976&itag=22&source=webdrive&requiressl=yes&ttl=transient&pl=19&sc=yes&ei=3HgcWejVN8bRqwX-86-oDA&driveid=0B7b7JSyiTZnMWFBDSjVlOUl4V2M&mime=video/mp4&lmt=1491605060830349&ip=122.167.118.210&ipbits=0&expire=1495052572&sparams=driveid,ei,expire,id,ip,ipbits,itag,lmt,mime,mm,mn,ms,mv,pl,requiressl,sc,source,ttl&signature=3BCDBDD1024A15BB8EE2384503F581722B8902B5.18422EFCBD7B47582E8AB386EE8A90DECF2E28EF&key=cms1&app=explorer&redirect_counter=1&req_id=309d17372df8a3ee&cms_redirect=yes&mm=30&mn=sn-h557snes&ms=nxu&mt=1495043188&mv=m'
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+#from pyvirtualdisplay import Display
 
-startingUrl = 'https://solarmoviez.to/movie/doctor-who-the-day-of-the-doctor-20226/615773-8/watching.html'
 
-wget.download(url, bar=bar_thermometer)
+import time
+
+
+'''
+- parse html of the page to get video link, name of file
+- download! and save name of file to txt file
+- navigate to another movie
+- repeat
+'''
+
+f = open('sites.txt', 'r+')
+f.seek(0,2)
+f.write('hi hi\n')
+f.seek(0,2)
+f.write('lol\n')
+f.seek(0,2)
+f.write('habadaaaduba3tu37786876423\n')
+f.close()
+
+
+"""
+Prints message in red.
+"""
+def print_error(message):
+    print '\033[31m{}\033[0m'.format(str(message))
+
+"""
+Prints message in yellow.
+"""
+def print_warning(message):
+    print '\033[33m{}\033[0m'.format(str(message))
+
+"""
+Prints message in green.
+"""
+def print_success(message):
+    print '\033[32m{}\033[0m'.format(str(message))
+
+
+
+
+
+#display = Display(visible=0, size=(800, 600))
+#display.start()
+#driver = webdriver.Chrome('/Users/Parth/Desktop/download-all/chromedriver')
+#driver = webdriver.PhantomJS(service_args=['--ssl-protocol=any'])  
+#driver.implicitly_wait(1) 
+
+#driver.get(startingUrl)
+
+#get movie page
+
+url = "https://solarmoviez.to/movie/the-accidental-spy-2014.html"
+
+movies = []
+
+
+while True:
+    driver = webdriver.Chrome('/Users/Parth/Desktop/download-all/chromedriver')
+    driver.get(url)
+
+    time.sleep(1)
+
+    nextMovieLink = driver.find_element_by_class_name('ml-mask').get_attribute("href")
+    print(nextMovieLink)
+
+
+    movieName = driver.find_element_by_tag_name("h3").get_attribute("innerText")
+    print("downloading " + str(movieName))
+    movies.append(movieName)
+
+    
+    movieLink = driver.find_element_by_class_name('bwac-btn').get_attribute("href")
+
+
+    driver.get(movieLink)
+
+    time.sleep(1)
+
+    downloadLink = driver.find_element_by_tag_name('video').get_attribute("src")
+    driver.close()
+
+
+    movieSave = str(movieName) + ".mp4"
+    wget.download(downloadLink, movieSave)
+
+    
+    print('DONE\n')
+
+    url = nextMovieLink
+
+
+
