495a0392f3f12846659046827270928d1b607bc3
Parth Shah <parthks@umich.edu>
2017-05-17 23:22:16 +0530

    First Commit

---
+++ run.py
@@ -0,0 +1,7 @@
+import wget
+
+final = 'https://r10---sn-h557snes.googlevideo.com/videoplayback?id=5b372923ff879976&itag=22&source=webdrive&requiressl=yes&ttl=transient&pl=19&sc=yes&ei=3HgcWejVN8bRqwX-86-oDA&driveid=0B7b7JSyiTZnMWFBDSjVlOUl4V2M&mime=video/mp4&lmt=1491605060830349&ip=122.167.118.210&ipbits=0&expire=1495052572&sparams=driveid,ei,expire,id,ip,ipbits,itag,lmt,mime,mm,mn,ms,mv,pl,requiressl,sc,source,ttl&signature=3BCDBDD1024A15BB8EE2384503F581722B8902B5.18422EFCBD7B47582E8AB386EE8A90DECF2E28EF&key=cms1&app=explorer&redirect_counter=1&req_id=309d17372df8a3ee&cms_redirect=yes&mm=30&mn=sn-h557snes&ms=nxu&mt=1495043188&mv=m'
+
+startingUrl = 'https://solarmoviez.to/movie/doctor-who-the-day-of-the-doctor-20226/615773-8/watching.html'
+
+wget.download(url, bar=bar_thermometer)
